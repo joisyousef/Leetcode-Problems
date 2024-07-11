b1d6dd10f6d7e704395b775cb4a5651b6dbcc170
Yousef Elsrogy <90921892+joisyousef@users.noreply.github.com>
2024-07-11 09:00:46 +0300

    Time: 40 ms (96.31%) | Memory: 49 MB (10.85%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -1,13 +1,18 @@
+/**
+ * @return {Function}
+ */
 function createHelloWorld() {
   return function() {
     return "Hello World";
   };
 }
-
-// Example 1:
 const f1 = createHelloWorld();
 console.log(f1()); // Output: "Hello World"
 
 // Example 2:
 const f2 = createHelloWorld();
 console.log(f2({}, null, 42)); // Output: "Hello World"
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
